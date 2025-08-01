7023c248ef0745ac86671b155ac5d2ea3af26b16
nikaydo <sooosooor49@gmail.com>
2025-08-01 19:36:49 +0500

    add env

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 
 	"github.com/nikaydo/fileStorage/internal/client"
@@ -13,20 +15,18 @@ import (
 
 func main() {
 	file := flag.String("file", "", "send file to server")
-	act := flag.String("mode", "", "action on server")
-	port := flag.String("port", "9000", "port to connect the server")
-	addr := flag.String("addr", "localhost", "the port through which the connection is made")
+	mode := flag.String("mode", "", "action on server")
 	flag.Parse()
-	if len(*file) == 0 && len(*act) == 0 {
-		log.Fatalln("u need chose file to send or actions")
+	if len(*mode) == 0 {
+		log.Fatalln("u need chose mode")
 		return
 	}
-	c, err := client.Connect(*port, *addr)
+	c, err := client.Connect(os.Getenv("PORT"), os.Getenv("ADDR"))
 	if err != nil {
 		log.Fatalln(err)
 	}
 	Message := client.Msg{WaitGroup: sync.WaitGroup{}, Clt: c}
-	switch *act {
+	switch *mode {
 	case "send":
 		if err := c.Write([]byte("send")); err != nil {
 			log.Println(err)
@@ -77,4 +77,38 @@ func main() {
 	default:
 		log.Fatalln("chose actions in act flag is not valid")
 	}
+}
+
+func loadenv(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+
+		if os.Getenv(key) != "" {
+			continue
+		}
+
+		os.Setenv(key, value)
+	}
+
+	return scanner.Err()
 }
